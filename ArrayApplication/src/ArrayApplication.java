a071edb94a54f20f5a450824044c93ceb2a6758d
arjoo <2009arjoo@gmail.com>
2024-09-24 00:09:07 +0530

    Added Array Application

--- ArrayApplication/src/ArrayApplication.java
+++ ArrayApplication/src/ArrayApplication.java
@@ -1,5 +1,40 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class ArrayApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        ArrayProgramme arrayProgramme = new ArrayProgramme();
+        //*************
+        // Linear Search
+        //*************
+        boolean linearFind = arrayProgramme.linearSearch(new int[]{1, 3, 7, 5, 9}, 3);
+        System.out.println(linearFind);
+
+        //*************
+        // Binary Search
+        //*************
+        boolean binary = arrayProgramme.binarySearch(new int[]{1, 3, 5, 7, 9} , 0, 4, 9);
+        System.out.println(binary);
+
+        //*************
+        // Bubble Sort
+        //*************
+        int[] arr = new int[]{1, 3, 5, 9, 7};
+        arr = arrayProgramme.bubbleSort(arr);
+        Arrays.stream(arr).forEach(e -> System.out.print(e +", "));
+        System.out.println();
+
+        arr = new int[]{1, 0, 5, 9, -1};
+        arr = arrayProgramme.bubbleSort(arr);
+        Arrays.stream(arr).forEach(e -> System.out.print(e +", "));
+        System.out.println();
+        //*************
+        // Insertion Sort
+        //*************
+        arr = new int[]{1, 0, 5, 9, -1};
+        arr = arrayProgramme.insertionSort(arr);
+        Arrays.stream(arr).forEach(e -> System.out.print(e +", "));
     }
 }
--- ArrayApplication/src/ArrayProgramme.java
+++ ArrayApplication/src/ArrayProgramme.java
@@ -1,2 +1,52 @@
 public class ArrayProgramme {
+
+    public boolean linearSearch(int[] arr, int elem) {
+        for (int j : arr) {
+            if (j == elem) {
+                return Boolean.TRUE;
+            }
+        }
+        return Boolean.FALSE;
+    }
+
+    public boolean binarySearch(int[] arr, int start, int end, int elem) {
+        if(start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == elem)
+                return Boolean.TRUE;
+            else if (arr[mid] > elem)
+                return binarySearch(arr, start, mid - 1, elem);
+            else
+                return binarySearch(arr, mid + 1, end, elem);
+        }
+        return Boolean.FALSE;
+    }
+
+    //{1, 3, 5, 9, 7}
+    public int[] bubbleSort(int[] arr) {
+        for(int i=0; i<arr.length; i++) {
+            for(int j=0; j< arr.length-1-i; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public int[] insertionSort(int[] arr) {
+        for(int i=1; i< arr.length; i++) {
+            int key = arr[i];
+            int j = i-1;
+            while (j>=0 && arr[j] > key) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+        return arr;
+    }
+
 }
