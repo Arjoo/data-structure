7b858598de4a63d4e2f446efde19462411b01d48
arjoo <2009arjoo@gmail.com>
2024-09-25 23:51:24 +0530

    Added changes

---
+++ StringApplication/src/StringApplication.java
@@ -0,0 +1,11 @@
+
+
+public class StringApplication {
+    public static void main(String[] args) {
+        StringProgramme stringProgramme = new StringProgramme();
+        String firstRepeated = stringProgramme.firstRepeated("india");
+        System.out.println("firstRepeated : " + firstRepeated);
+
+
+    }
+}
---
+++ StringApplication/src/StringProgramme.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StringProgramme {
+
+    public String firstRepeated(String str) {
+        Map.Entry<String, Long> findFirst = Arrays.stream(str.split(""))
+                .collect(
+                        Collectors.groupingBy(
+                                Function.identity(), LinkedHashMap::new, Collectors.counting()
+                        )
+                )
+                .entrySet()
+                .stream()
+                .filter(e -> e.getValue() == 1)
+                .findFirst().get();
+
+        return findFirst.getKey();
+    }
+}
