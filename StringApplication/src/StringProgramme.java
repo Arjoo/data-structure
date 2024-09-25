c909e7d5a519b2af9eac021a63fa57ee9e1b2368
arjoo <2009arjoo@gmail.com>
2024-09-25 23:55:18 +0530

    Added changes

--- StringApplication/src/StringProgramme.java
+++ StringApplication/src/StringProgramme.java
@@ -19,4 +19,14 @@ public class StringProgramme {
 
         return findFirst.getKey();
     }
+
+    public String reverse(String str) {
+        char[] chars = str.toCharArray();
+        for(int i=0, j=chars.length-1 ; i<j; i++, j--) {
+            char temp = chars[i];
+            chars[i] = chars[j];
+            chars[j] = temp;
+        }
+        return chars.toString();
+    }
 }
