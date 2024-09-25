060ba66f196ee93d1fd4dd8c8be4335b92b3beb7
arjoo <2009arjoo@gmail.com>
2024-09-25 23:57:11 +0530

    Added changes

--- StringApplication/src/StringApplication.java
+++ StringApplication/src/StringApplication.java
@@ -3,9 +3,16 @@
 public class StringApplication {
     public static void main(String[] args) {
         StringProgramme stringProgramme = new StringProgramme();
+
+        //******************
+        // First Repeated
+        //******************
         String firstRepeated = stringProgramme.firstRepeated("india");
         System.out.println("firstRepeated : " + firstRepeated);
 
+        //******************
+        // Reverse a string
+        //******************
         String reverse = stringProgramme.reverse("indian");
         System.out.println("reverse : " + reverse);
 
